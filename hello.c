9cf37bfa5e2889fcc94de148d23370cd4ac578a7
bonggba <31675685+bonggba@users.noreply.github.com>
2017-09-10 13:16:30 +0900

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main(){
+	printf("Student ID : 2017301046\n");
+	printf("Name : Yeo bong jun\n");
+	printf("Phone number : 010 5017 6417\n");
+	printf("Github address : duqhdwns@naver.com\n");
+return 0;
+}
